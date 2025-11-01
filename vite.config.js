f11d808413c689f53c7013935f699b583bf1af86
skfirdous2323-afk <skfirdous2323@gmail.com>
2025-11-01 21:38:00 +0530

    fix: allow render host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ['xefere-chatbot.onrender.com'], // ✅ ye line add karo
+  },
+});
+
